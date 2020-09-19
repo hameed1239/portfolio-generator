1710ae7420f410b29a0d16156fea7c6ee5c48cb4
hameed kazeem <hameed.kazeem@gmail.com>
2020-09-19 06:31:22 -0400

    add feature to generate webpage

--- apps.js
+++ apps.js
@@ -1,14 +1,26 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++){
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++){
         
-        console.log(profileDataArr[i]);
-        console.log("=======================");
-        profileDataArr.forEach(profileItem => console.log(profileItem));
-    }
-};
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log("=======================");
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
 
+
+
+// console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
