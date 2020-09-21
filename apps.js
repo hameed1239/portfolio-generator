7033767b178e3835a980f54c8ebbc70b04a14f33
hameed kazeem <hameed.kazeem@gmail.com>
2020-09-20 23:10:33 -0400

    Complete template creation

--- apps.js
+++ apps.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const generatePage = require('./src/page-template');
-
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 // fs.writeFile('index.html', generatePage(name, github), err => {
 //     if (err) throw err;
 //     console.log('Portfolio complete! Check out index.html to see the output!')
@@ -39,9 +39,25 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself: ',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            },
+            when: ({ confirmAbout }) => confirmAbout
         }
 
     ]);
@@ -118,27 +134,6 @@ Add a New Project
             name: 'confirmAddProject',
             message: 'Would you like to enter another project?',
             default: false
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAbout',
-            message: 'Would you like to enter some information about yourself for an "About" section?',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'about',
-            message: 'Provide some information about yourself: ',
-            validate: linkInput => {
-                if (linkInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter your name!');
-                    return false;
-                }
-            },
-            when: ({confirmAbout}) => confirmAbout
         }
     ])
         .then(projectData => {
@@ -152,50 +147,21 @@ Add a New Project
         })
 }
 
-const mockData = {
-    name: 'hameed',
-    github: 'hmeed1239',
-    about: 'I love coding',
-    projects: [
-        {
-            name: 'portfolio generator',
-            description: 'portfolio generator  krngkg pgsklrgnrough rnng spjgwrngjnd; d;jjs; g;sotgh lig rtgtrlhg rtogh tr;g tjghhg iot ghtoeighteghtegh iojhuihtgiohjo  oihhuohoitheg iohuohyoiet oihoteihgyoejtgoie ;eoihoetgeioj s;he;ghlguht98 luhgotgheto spjgwrngjnd; d;jjs; g;sotgh lig rtgtrlhg rtogh tr;g tjghhg iot ghtoeighteghtegh iojhuihtgiohjo  oihhuohoitheg iohuohyoiet oihoteihgyoejtgoie ;eoihoetgeioj s;he;ghlguht98 luhgotgheto  spjgwrngjnd; d;jjs; g;sotgh lig rtgtrlhg rtogh tr;g tjghhg iot ghtoeighteghtegh iojhuihtgiohjo  oihhuohoitheg iohuohyoiet oihoteihgyoejtgoie ;eoihoetgeioj s;he;ghlguht98 luhgotgheto  ',
-            languages: ['Java','CSS','Node','JavaScript','HTML',],
-            link: 'https://hameed.kazeem',
-            feature: true,
-            confirmAddProject: true,
-            confirmAbout: true,
-            about: 'This is the only project that should have an about info'
-        },
-        {
-            name: 'Weather map',
-            description: 'Weather map',
-            languages: ['Java','CSS','JavaScript','HTML',],
-            link: 'https://hameed.kazeem',
-            feature: true,
-            confirmAddProject: true,
-            confirmAbout: false
-        },
-        {
-            name: 'Robot Gladiators',
-            description: 'Robot Gladiators kdmvdjo  pojagvkjfpja jgaj grjnpgi anjd gpn gkd;gouet n gijirjfsdkfjrsf  srlgksrj gjsrjfsklgnv spjgwrngjnd; d;jjs; g;sotgh lig rtgtrlhg rtogh tr;g tjghhg iot ghtoeighteghtegh iojhuihtgiohjo  oihhuohoitheg iohuohyoiet oihoteihgyoejtgoie ;eoihoetgeioj s;he;ghlguht98 luhgotgheto  spjgwrngjnd; d;jjs; g;sotgh lig rtgtrlhg rtogh tr;g tjghhg iot ghtoeighteghtegh iojhuihtgiohjo  oihhuohoitheg iohuohyoiet oihoteihgyoejtgoie ;eoihoetgeioj s;he;ghlguht98 luhgotgheto isrjgskdjgmpsjsgn s srogjs prjp gjsrjgs',
-            languages: ['Java','CSS','Node','JavaScript','HTML',],
-            link: 'https://hameed.kazeem',
-            feature: false,
-            confirmAddProject: false,
-            confirmAbout: false
-        }
-    ]
-};
-// promptUser()
-//     .then(promptProject)
-    // .then(portfolioData => {
-        // const pageHTML = generatePage(portfolioData);
-        const pageHTML = generatePage(mockData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log('Page created! Check out index.html in this directory to see it!');
-        });
-    // });
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
